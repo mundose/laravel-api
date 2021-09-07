b03ea63951a61f8c55c0647e5845ed34eb1cbe22
José <joseamietta@gmail.com>
2021-09-07 13:29:34 -0300

    user faker

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        // $user = factory(App\User::class, 20)->create();
+        // $this->call([UsersTableSeeder::class]);
     }
 }
